8392d70a093ce4dfc7a669babc637ad263b96a28
MiHyeon <kkmmhh7478@gmail.com>
2024-08-31 19:46:30 +0900

    #1 nav바 style 수정 및 검색 버튼 추가

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from 'react-router-dom';
 const AppLayout = () => {
     return (
         <div data-theme="dracula">
-            <div className="navbar bg-primary text-white">
+            <div className="navbar bg-neutral text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -39,9 +39,23 @@ const AppLayout = () => {
                 </div>
                 <div className='navbar-end hidden lg:flex'>
                     <div className="flex-none gap-2">
-                        <div className="form-control">
+                        <form className="flex gap-1">
                             <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-                        </div>
+                            <button type='submit' className="btn btn-primary btn-square btn-outline">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
