16feff5f2a3a7bbdf3d9fa92d54abb66c99240c0
MiHyeon <kkmmhh7478@gmail.com>
2024-09-06 00:02:32 +0900

    #1 #10 detail page design 수정

--- src/common/PosterCard/PosterCard.jsx
+++ src/common/PosterCard/PosterCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./PosterCard.style.css"
-
+import { useNavigate } from 'react-router-dom';
 import { useMovieGenresQuery } from '../../hooks/useMovieGenres';
 import { useTvGenresQuery } from '../../hooks/useTvGenres';
 
@@ -8,44 +8,48 @@ const PosterCard = ({ item }) => {
     const imgSrc = `https://image.tmdb.org/t/p/original/${item?.poster_path}`
     const { data: movieGenres } = useMovieGenresQuery();
     const { data: tvGenres } = useTvGenresQuery();
+    const navigate = useNavigate();
 
-    let genres;
+    let genres, type;
 
-    if (item?.title) genres = movieGenres;
-    if (item?.name) genres = tvGenres;
+    if (item.title) {
+        genres = movieGenres;
+        type = 'movie'
+    }
+    if (item.name) {
+        genres = tvGenres;
+        type = 'tv'
+    }
 
-    const cardTitle = item.title 
-    ? item.title.length > 30
-        ? item.title.slice(0, 30) + "..."
-        : item.title
-    : item.name.length > 30
-        ? item.name.slice(0, 30) + "..."
-        : item.name;
+    const cardTitle = item.title
+        ? item.title.length > 30
+            ? item.title.slice(0, 30) + "..."
+            : item.title
+        : item.name.length > 30
+            ? item.name.slice(0, 30) + "..."
+            : item.name;
 
     return (
-        <div>
-            <div className='test h-full'>
-                {item.poster_path === null ? <div className='w-full h-full bg-secondary rounded-lg' />
-                    : <figure><img src={imgSrc} alt="card image" className='rounded-lg' /></figure>}
-                <div className='overlay p-10 flex flex-col h-full'>
-                    <h2 className="card-title text-xl mb-2">{cardTitle}</h2>
-                    <div className='flex mb-2'>
-                            {item.genre_ids.map((id, index) => {
-                                if (index === 0) {
-                                    const genre = genres?.find((g) => g.id === id).name.split(" ", 1);  // 장르 ID와 매칭되는 이름 찾기
-                                    return genre ? (
-                                        <div key={index} className="badge badge-primary mr-2">
-                                            {genre}
-                                        </div>
-                                    ) : null;  // 장르가 없으면 null 반환                                    
-                                }
-                            })}
-                        </div>
-                    <div className='mb-2'>{Array(Math.round(item.vote_average / 2)).fill('⭐')}</div>
-                    <div>{item.adult ? <div className="badge badge-error gap-2">18+</div> : <div className="badge badge-success gap-2">All</div>}</div>
+        <div className='test h-full' onClick={()=>navigate(`/detail/${type}/${item?.id}`)}>
+            {item.poster_path === null ? <div className='w-full h-full bg-secondary rounded-lg' />
+                : <figure><img src={imgSrc} alt="card image" className='rounded-lg' /></figure>}
+            <div className='overlay p-10 flex flex-col h-full'>
+                <h2 className="card-title text-xl mb-2">{cardTitle}</h2>
+                <div className='flex mb-2'>
+                    {item.genre_ids.map((id, index) => {
+                        if (index === 0) {
+                            const genre = genres?.find((g) => g.id === id).name.split(" ", 1);  // 장르 ID와 매칭되는 이름 찾기
+                            return genre ? (
+                                <div key={index} className="badge badge-primary mr-2">
+                                    {genre}
+                                </div>
+                            ) : null;  // 장르가 없으면 null 반환                                    
+                        }
+                    })}
                 </div>
+                <div className='mb-2'>{Array(Math.round(item.vote_average / 2)).fill('⭐')}</div>
+                <div>{item.adult ? <div className="badge badge-error gap-2">18+</div> : <div className="badge badge-success gap-2">All</div>}</div>
             </div>
-
         </div>
 
     )
--- src/pages/Detail/DetailPage.jsx
+++ src/pages/Detail/DetailPage.jsx
@@ -8,6 +8,8 @@ const DetailPage = () => {
 
   const details = data?.data;
 
+  const stars = Math.round(details?.vote_average / 2);
+
   // 로딩 상태일 때 로딩 UI를 반환
   if (isLoading) {
     return <span className="loading loading-bars loading-lg text-primary"></span>;
@@ -44,8 +46,8 @@ const DetailPage = () => {
 
         <div className="absolute top-[68px] left-0 w-full h-96 p-16 bg-opacity-50 flex items-end justify-start">
           <div className="text-white text-start">
-            <h1 className="text-4xl font-bold mb-4">{details.title || details.name}</h1>
-            <p className='flex'>
+            <h1 className="text-4xl font-bold mb-4 text-primary">{details.title || details.name}</h1>
+            <p className='flex max-sm:flex-col'>
               {(details.release_date ? details.release_date.slice(0, 4) : '') ||
                 (details.first_air_date ? details.first_air_date.slice(0, 4) : '')}
               &nbsp;·&nbsp; {details.genres.map((genre, index) => {
@@ -60,14 +62,23 @@ const DetailPage = () => {
       </>
 
       <div className='flex w-auto h-auto lg:mx-40 lg:flex-row justify-center gap-8 mx-10 flex-col'>
-        <img
-          className='h-[400px]'
-          src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
-          alt={details.title || details.name}
-        />
-        <div className='flex flex-col justify-center gap-4'>
-          <div className='text-4xl mb-2'>{Array(Math.round(details.vote_average / 2)).fill('⭐')}</div>
-          <div className='flex mb-2'>
+        <div className='flex flex-col gap-1 w-[238px] flex-shrink-0'>
+          <img
+            className='h-[400px] w-[238px] object-cover mb-2'
+            src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
+            alt={details.title || details.name}
+          />
+          <h2 className='text-xl font-semibold text-secondary border-b-secondary border-b-2 pb-2'>RATING</h2>
+          <div className='text-4xl mb-2 flex'>
+            {Array(stars).fill(<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Star.png" alt="Star" width="36" height="36" />)}
+            {Array(5 - stars).fill(<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Milky%20Way.png" alt="Milky Way" width="36" height="36" />)}
+          </div>
+          <p>Average⭐{details.vote_average}&nbsp;</p>
+          <p className='text-neutral-content text-sm font-thin'>({details.vote_count} people rated)</p>
+        </div>
+
+        <div className='flex flex-col gap-4'>
+          <div className='flex mb-2 max-sm:flex-col max-sm:gap-4'>
             {details.genres.map((genre, index) => {
               return <div key={index} className="badge badge-primary mr-2">
                 {genre.name}
@@ -75,12 +86,12 @@ const DetailPage = () => {
             })}
           </div>
           <h2 className='text-xl font-semibold text-secondary border-b-secondary border-b-2 pb-2'>OVERVIEW</h2>
+          {details.tagline ? <p className='mb-1 text-primary'>"{details.tagline}"</p> : ''}
           <p>{details.overview}</p>
           <h2 className='text-xl font-semibold text-secondary border-b-secondary border-b-2 pb-2'>REALEASE DATE</h2>
           <p>{details.release_date || details.first_air_date}</p>
           <h2 className='text-xl font-semibold text-secondary border-b-secondary border-b-2 pb-2'>RUNTIME</h2>
           <p>{details.runtime || details.episode_run_time?.[0]} minutes</p>
-          <p>Rating: {details.vote_average}</p>
         </div>
       </div>
     </div>
